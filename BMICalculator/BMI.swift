a84fdc963cbafd2399ae7f43ff4db6076ec8267d
emmaderbe <emma-derbe@mail.ru>
2023-02-05 20:45:34 +0300

    first commit

---
+++ BMICalculator/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Дербе Эмма on 05.02.2023.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
---
+++ BMICalculator/ResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultViewController.swift
+//  BMICalculator
+//
+//  Created by Дербе Эмма on 05.02.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
